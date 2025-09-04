ae3c1a8981c4f26acef92906ae27e2d537de8512
parvej19alam <parvej19.alam@gmail.com>
2025-09-04 16:09:44 +0530

    Add files via upload

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,23 @@
+
+
+const accountID = 13133
+let accountEmail = "parvej.alam@codeclouds.in"
+var accountPassword = "123456789"
+accounCity = "Kolkata"
+let accountState;
+
+//accountID = 2 //Not allowed
+accountEmail = "pa@pa.com"
+accountpassword = "11223344"
+accountCity = "Patna"
+
+
+//console.log(accountID)
+
+/*
+Prefer not to use var
+Beacuse of issue in block scope and functional scope
+*/
+console.table([accountID, accountEmail, accountpassword, accountCity, accountState])
+
+
---
+++ 01_basic/test.js
@@ -0,0 +1,2 @@
+console.log("Hello this is first console")
+console.log("Hello This is parvej")
